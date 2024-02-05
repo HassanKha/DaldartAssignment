076ec8eea7aa4d855670cedcfdc3adb4e2aa6c0d
HassanKha <hassankhaled4432@gmail.com>
2024-02-06 00:26:29 +0200

    equesting and storing data from Reddit's API

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # DaldartAssignment
+
+Creating a backend application using Node.js and the Express framework to build an API for fetching data from the Reddit API about the subreddit "wwe"
+
+Setting Up the Project:
+Initialize a New Node.js Project:
+
+1- Create a new directory for your project.
+Run npm init to initialize a new Node.js project, and follow the prompts to set up the project details.
+
+2- Install necessary dependencies, including express for building the web server and axios for making HTTP requests.
+
+3- Set up the main server file (e.g., app.js or server.js).
+Require express and create an instance of the application.
+
+4- Configure middleware for handling JSON requests and handling CORS.
+
+5- Use the axios library to make requests to the Reddit API.
+
+6- Enable Cross-Origin Resource Sharing (CORS) to allow requests from your frontend application.
+
+7- Set up Firebase to store and retrieve data from the Reddit API.
+Install the Firebase SDK and configure it.
---
+++ server/index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import axios from "axios";
+import firebase from "./firebase.js";
+import { getFirestore, collection, doc, getDoc, setDoc, getDocs } from 'firebase/firestore';
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+
+const headers = {
+    'Content-Type': 'application/json',
+  "User-Agent": "HassanKha4432",
+  Authorization:
+    "bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzA3MjQxNDY0LjkyNDA2MSwiaWF0IjoxNzA3MTU1MDY0LjkyNDA2MSwianRpIjoiQzdSNkItM3dNaVd6ZFdsS1RYTUl5ZUIxQWl6S05nIiwiY2lkIjoiZUxDTmlwMmRIZ0lNdlBtU3hZTEZwUSIsImxpZCI6InQyX3J2ZGZxY3h1OSIsImFpZCI6InQyX3J2ZGZxY3h1OSIsImxjYSI6MTcwNTAwMjQ4NjA1Mywic2NwIjoiZUp5S1Z0SlNpZ1VFQUFEX193TnpBU2MiLCJmbG8iOjl9.Q1n8bOqjVUMVkWloZC7D1JxzNxrQCLu3ClmtRbTk1R-jN8bkFDGdggSOgLDP1FR-8xW17sQQc5TVuyb89R_vpA2lEYAsqlcAhquF7aP9psUojoBl2jSta2jTDEKqvh5pQSQZ9MN7ybCNgJwe4gtJuMIv1DsxAZeBHgpZOn-LLoS0_dwSAgv50SR2twczM2nlnI-c4ctjqaaMauZReWAxTaULQnm34weuqCYlNkXWEWcco61tox_mT1bw4KB3fz1prdJnmcwfM6RoW-KWcIc9ankOsb6OLqyEDKBFXAtyOlBHKGZqKAibkSSigFTcDkbYgb3TcCrQtnp72Af4OybglQ",
+};
+
+
+const axiosConfig = {
+  headers,
+};
+
+
+
+app.post("/fetch-and-store", async (req, res) => {
+
+  try {
+    
+    const redditApiUrl =
+      `https://oauth.reddit.com/r/wwe/${req.body.Type}?after=` + req.body.data;
+
+      console.log(req.body,redditApiUrl);
+
+    const response = await axios.get(redditApiUrl, axiosConfig);
+
+    const responseData = {
+      status: response.status,
+      statusText: response.statusText,
+      data: response.data,
+    };
+
+    const dataFromReddit = responseData.data;
+    const children = dataFromReddit.data;
+ 
+    res.json({ success: true, children: children });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
